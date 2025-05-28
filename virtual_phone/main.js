9f25499eaaf8911050995047d5ba48ffa24c7224
giovanabordinhonreis <giovanabordinhon@gmail.com>
2025-05-27 21:42:24 -0300

    Virtual Phone final 1.2

--- virtual_phone/main.js
+++ virtual_phone/main.js
@@ -5,7 +5,7 @@ const teclas = document.querySelectorAll('input[type=button]');
 const limpar = document.querySelector('.icon');
 
 limpar.onclick = () => {
-    const opcao = confirm("Vai limpar mesmo, porra????");
+    const opcao = confirm("Tem certeza que deseja limpar?");
     if(opcao){
         display.value = "";
     }
@@ -15,7 +15,7 @@ function valorTeclaToDisplay (i) {
     if (display.value.length < 14) {
         display.value += teclas[i].value;
     } else {
-        alert("Para de digitar, porra!!!")
+        alert("Chegou no limite de dígitos!")
     }
 }
 
