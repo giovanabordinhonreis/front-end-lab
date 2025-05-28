ccb1a4636af3849635e3b5cffeaa81a3983a80ff
giovanabordinhonreis <giovanabordinhon@gmail.com>
2025-05-27 21:38:02 -0300

    Virtual Phone final

--- virtual_phone/main.js
+++ virtual_phone/main.js
@@ -5,15 +5,18 @@ const teclas = document.querySelectorAll('input[type=button]');
 const limpar = document.querySelector('.icon');
 
 limpar.onclick = () => {
-    const opcao = confirm("Deseja mesmo limpar?");
+    const opcao = confirm("Vai limpar mesmo, porra????");
     if(opcao){
         display.value = "";
     }
 }
 
-//Tarefinha: Criar uma condição para travar mais de 14 digitos
 function valorTeclaToDisplay (i) {
-    display.value += teclas[i].value;
+    if (display.value.length < 14) {
+        display.value += teclas[i].value;
+    } else {
+        alert("Para de digitar, porra!!!")
+    }
 }
 
 for (let i = 0; i < teclas.length; i++){
